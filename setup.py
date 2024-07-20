5a03428c6d0dec87844881d54566cb4189563891
rudyarthur <rudy.d.arthur@gmail.com>
2024-07-20 14:03:02 +0100

    numpy int bug

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
+import numpy
 
 extensions = [
-    Extension("valeriepieris", ["src/valeriepieris.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),
+    Extension("valeriepieris", ["src/valeriepieris.pyx"], define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],include_dirs=[numpy.get_include()]),
 ]
 setup(
    ext_modules = cythonize(extensions, annotate=True, 
